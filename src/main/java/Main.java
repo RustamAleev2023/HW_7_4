693e49fdbe600bceda1004ce2ff8f757eb65dddb
RustamAleev2023 <ridermix@yandex.ru>
2024-01-23 13:38:11 +0200

    Task2, Task3 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+//        task1();
+//        task2();
+        task3();
     }
     //Задание 1
     //Создать два конструктора.
@@ -12,10 +14,107 @@ public class Main {
     //size = 0). Переиспользовать конструктор с параметрами
     //для уменьшения кода.
     public static void task1(){
-        MyArrayList arrayList1 = new MyArrayList<Integer>(5);
+        MyArrayList<Integer> arrayList1 = new MyArrayList<Integer>(5);
         System.out.println(arrayList1);
-        MyArrayList arrayList2 = new MyArrayList<Integer>();
+        MyArrayList<Integer> arrayList2 = new MyArrayList<Integer>();
         System.out.println(arrayList2);
+
+    }
+
+    //Задание 2
+    //Реализовать методы:
+    //■ геттеры для size. Сеттера для size не должно быть!
+    //■ переопределить метод toString и реализовать строковое
+    //представление элементов массива через пробел.
+    //■ ensureCapacity – закрытый метод! проверяет, достаточно
+    //ли резерва памяти для хранения указанного в параметре
+    //количества элементов. Если значение параметра меньше
+    //текущего capacity, то ничего не происходит. Если значение
+    //параметра больше текущего capacity, томассив пересоздается, памяти выделяется в 1,5 раза + 1 элемент больше.
+    //Существующие элементы переносятся в новый массив.
+    //Существующие элементы не должны быть потеряны.
+    public static void task2(){
+        MyArrayList<Integer> arrayList1 = new MyArrayList<Integer>(3);
+        System.out.println(arrayList1.getSize());
+
+
+    }
+
+    //Задание 3
+    //Реализовать методы:
+    //■ pushBack (добавление элемента в конец массива. Должна
+    //быть проверка, достаточно ли памяти! Если памяти не
+    //достаточно увеличить емкость массива данных);
+    //■ popFront (удаление первого элемента из массива);
+    //■ pushFront (добавление нового элемента в начало массива);
+    //■ insert (вставка нового элемента в массив по указанному
+    //индексу, с проверкой на выход за пределы массива);
+    //■ removeAt(удаление одного элемента по указанному индексу.
+    //Должна быть проверка на допустимость индекса);
+    //■ remove (удаление одного элемента, значение которого
+    //совпадает со значением переданного параметра);
+    //3
+    //■ removeAll (удаление всех элементов, значения которых
+    //совпадает со значением переданного параметра);
+    //■ popBack (удаление последнего элемента из массива);
+    //■ сlear (обнуление массива – всем элементам массива по
+    //индексам от 0 до size-1 присвоить значение null, полю size
+    //присвоить значение 0).
+    public static void task3(){
+        MyArrayList<Integer> arrayList1 = new MyArrayList<Integer>(3);
+        System.out.println(arrayList1.getSize());
+        arrayList1.pushBack(1);
+        System.out.println(arrayList1.getSize());
+        System.out.println(arrayList1);
+        System.out.println("=======================");
+        arrayList1.pushBack(2);
+        System.out.println(arrayList1.getSize());
+        System.out.println(arrayList1);
+        System.out.println("=======================");
+        arrayList1.pushBack(3);
+        System.out.println(arrayList1.getSize());
+        System.out.println(arrayList1);
+        System.out.println("=======================");
+        arrayList1.pushBack(4);
+        System.out.println(arrayList1);
+        System.out.println("=======================");
+        arrayList1.popFront();
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.pushFront(1);
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.insert(5,2);
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.removeAt(2);
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.remove(2);
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.insert(3,2);
+        arrayList1.insert(3,2);
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.removeAll(3);
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.popBack();
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+        System.out.println("=======================");
+        arrayList1.clear();
+        System.out.println(arrayList1);
+        System.out.println(arrayList1.getSize());
+
 
     }
 }
--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.StringJoiner;
 
@@ -9,14 +9,12 @@ public class MyArrayList<T> {
 
     public MyArrayList(int capacity) {
         this.capacity = capacity;
-        this.data =(T[]) new Object[capacity];
+        this.data = (T[]) new Object[capacity];
     }
 
     public MyArrayList() {
         this(10);
-        for (int i = 0; i < data.length; i++) {
-            data[i] = null;
-        }
+        Arrays.fill(data, null);
     }
 
     public int getSize() {
@@ -32,10 +30,112 @@ public class MyArrayList<T> {
         return joiner.toString();
     }
 
-    private void ensureCapacity(int size) {
-        if (size > capacity) {
+    public void ensureCapacity(int amount) {
+        if (amount > capacity - getSize()) {
             capacity = (int) (capacity * 1.5 + 1);
             data = Arrays.copyOf(data, capacity);
         }
+    }
+
+    public void pushBack(T element) {
+        if (size >= capacity) {
+            ensureCapacity(size);
+        }
+        data[size] = element;
+        size++;
+    }
+
+    public void popFront() { //удаляем первый элемент в массиве. После удаления, все элементы сдвагаем влево.
+        for (int i = 1; i < data.length; i++) {
+            data[i - 1] = data[i];
+        }
+        data[data.length - 1] = null;
+        size--;
+    }
+
+    public void pushFront(T element) {
+        int oldCapacity = size;
+        if (size == capacity) {
+            ensureCapacity(1);
+        }
+        for (int i = oldCapacity; i > 0; i--) {
+            data[i] = data[i - 1];
+        }
+        data[0] = element;
+        size++;
+    }
+
+    public void insert(T element, int index) {
+        if (!(index > 0 && index < data.length)) {
+            System.out.println("Такого индекса нет в массиве");
+        } else {
+            if (size == capacity) {
+                ensureCapacity(1);
+            }
+            for (int i = size; i > index; i--) {
+                data[i] = data[i - 1];
+            }
+            data[index] = element;
+        }
+        size++;
+    }
+
+    public void removeAt(int index) {
+        if (!(index > 0 && index < data.length)) {
+            System.out.println("Такого индекса нет в массиве");
+        } else {
+            for (int i = index; i < data.length - 1; i++) {
+                data[i] = data[i + 1];
+            }
+            data[data.length - 1] = null;
+        }
+        size--;
+    }
+
+    public void remove(T element) {
+        int index = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == element) {
+                index = i;
+                break;
+            }
+        }
+        removeAt(index);
+    }
+
+    public void removeAll(T element) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == element) {
+                data[i] = null;
+                size--;
+            }
+        }
+        int i = 0;
+        int j = 0;
+        while (j < data.length) {
+            if (data[i] == null && data[j] != null) {
+                data[i] = data[j];
+                data[j] = null;
+            }
+            if (data[i] != null) {
+                i++;
+            }
+            j++;
+        }
+    }
+
+    public void popBack() {
+        if(data[data.length - 1] != null){
+            size--;
+        }
+        data = Arrays.copyOf(data, data.length - 1);
+
+    }
+
+    public void clear(){
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
     }
 }
